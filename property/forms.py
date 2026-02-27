562bcbb57a37f92c901e0faece85593541347600
ORION <alka.pandey1031986@gmail.com>
2026-02-27 16:33:36 +0530

    Feature: Integrated Geolocation and Geocoding for Property maps

--- property/forms.py
+++ property/forms.py
@@ -4,10 +4,10 @@ from .models import Property
 class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
-        fields = ['title', 'description', 'price', 'property_type', 'address', 'city', 'bedrooms', 'bathrooms', 'sqft']
+        fields = ['title', 'description', 'price', 'property_type', 'address', 'city', 'bedrooms', 'bathrooms', 'sqft', 'latitude', 'longitude']
         widgets = {
             field: forms.TextInput(attrs={'class': 'w-full px-4 py-3 bg-slate-50 border border-slate-200 rounded-xl outline-none focus:ring-4 focus:ring-blue-100 transition-all font-medium'}) 
-            for field in ['title', 'price', 'address', 'city', 'bedrooms', 'bathrooms', 'sqft']
+            for field in ['title', 'price', 'address', 'city', 'bedrooms', 'bathrooms', 'sqft', 'latitude', 'longitude']
         }
 
     def __init__(self, *args, **kwargs):
