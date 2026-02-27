06044c523a2256f3235f44ff40afcfa73fe7cb77
ORION <alka.pandey1031986@gmail.com>
2026-02-27 17:42:04 +0530

    UI: Integrated Full View buttons in detail and Marketplace

--- map/views.py
+++ map/views.py
@@ -3,7 +3,9 @@ from property.models import Property
 
 def fullscreen_map(request):
     """View for full screen map showing all/selected properties."""
+    focus_id = request.GET.get('focus')
     properties = Property.objects.exclude(latitude__isnull=True).exclude(longitude__isnull=True)
     return render(request, 'map/fullscreen_map.html', {
-        'properties': properties
+        'properties': properties,
+        'focus_id': focus_id
     })
