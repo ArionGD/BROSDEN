2f8c6777c88d35ef5f0d9e46bf40039a1d5ddc5c
ORION <alka.pandey1031986@gmail.com>
2026-03-15 13:35:36 +0530

    Fixed missing settings import in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from .forms import UserRegistrationForm, KYCForm
 from .models import KYC, ProMembership
 from .ocr_utils import verify_id_document
